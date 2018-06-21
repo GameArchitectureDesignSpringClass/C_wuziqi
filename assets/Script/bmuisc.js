b922c2d002ab9af51aadec73aa3109532ce1ace5
zk571 <719129139@qq.com>
2018-06-21 11:09:19 +0800

    五子棋

---
+++ assets/Script/bmuisc.js
@@ -0,0 +1,29 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    properties: {
+         audioSource: {
+             type: cc.AudioSource,
+             default: null
+         },
+    },
+    play: function () {
+         this.audioSource.play();
+    },
+    pause: function () {
+         this.audioSource.pause();
+    },
+    }); 
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+   
